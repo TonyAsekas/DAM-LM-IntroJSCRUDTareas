47590afa384dfc0d7e8532e38847ecd1c1784556
Antonio Álvarez Fernández <toni12tineo@gmail.com>
2026-04-29 20:23:40 +0200

    actu

---
+++ app.js
@@ -0,0 +1,24 @@
+
+nombre = "Juan";
+var nombre;
+console.log("nombre: ", nombre)
+
+let num = 4;
+let num2 = 5;
+let resultado = num1 + "" + num2;
+console.log(resultado);
+
+const PI = 3.1416;
+
+const frutas = ["manzana", "pera"];
+frutas = ["peras"];
+
+let usuario = null;
+console.log(usuario);
+
+let alumno = {
+    dni: "1234A",
+    nombre:"Pepe",
+    edad:20,
+    aprobado: true
+};
